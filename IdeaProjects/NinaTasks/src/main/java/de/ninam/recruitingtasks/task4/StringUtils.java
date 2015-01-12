0b82582bffcc7e6736dbca5b2a4c1a85bcb59cfb
nmuelle2 <nina-marie.mueller@web.de>
2015-01-12 14:09:48 +0100

    Tasks 1-5 working...

---
+++ IdeaProjects/NinaTasks/src/main/java/de/ninam/recruitingtasks/task4/StringUtils.java
@@ -0,0 +1,61 @@
+package de.ninam.recruitingtasks.task4;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Helps with operations on Strings
+ */
+@Component
+public class StringUtils {
+
+    /**
+     * Finds all Characters in a given String and returns them unique (order preserved)
+     *
+     * @param someString String that Characters should be searched in
+     * @return all Characters in a given String and returns them unique (order preserved)
+     */
+    public List<Character> Characters(String someString) {
+
+        // character list
+        List<Character> CharList = new ArrayList<Character>();
+
+        // add characters
+        for (Character c : someString.toCharArray()) {
+
+            // only add character if not yet included (avoid duplicates)
+            if (!CharList.contains(c)) {
+                CharList.add(c);
+            }
+        }
+        return CharList;
+    }
+
+    /**
+     * Performs a search for a given Character in a given String
+     *
+     * @param analyzeMe String that the letter should be searched in
+     * @return count of Character in String
+     */
+    public int countLetter(String analyzeMe) {
+
+        // character count
+        int count = 0;
+        char current = analyzeMe.charAt(0);
+
+        // iterate over all characters in string
+        for (char c : analyzeMe.toCharArray()) {
+
+            // increase count if character matches
+
+            if (c == current) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+}
